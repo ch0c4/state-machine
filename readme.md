feb9c19be7feb81b88b0e6a8f8865e3d0027742d
Johan Sampedro <j.sampedro-ext@cultura.fr>
2024-08-27 09:29:19 +0200

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Godot finite state machine
+
+All credits goes to 
+ * https://github.com/Bitlytic
+ * https://www.youtube.com/watch?v=ow_Lum-Agbs
---
+++ state.gd
@@ -0,0 +1,19 @@
+class_name State extends Node
+
+@warning_ignore("unused_signal")
+signal transitionned
+
+func enter() -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func update(_delta: float) -> void:
+	pass
+
+
+func physics_update(_delta: float) -> void:
+	pass
